d8df6ac7716ec17fb930a63ddb63665566b70b9b
MiLo <milo-software@users.sourceforge.net>
2011-11-12 19:45:06 +0100

    setup.py: Add locale files
    Hmm, need to find the obvious way to compile .po->.mo from distutils

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup (name = 'enigma2-plugin-extensions-foreca',
        description = 'Foreca - Weather forecast',
        packages = [pkg],
        package_dir = {pkg: 'plugin'},
-       package_data = {pkg: ['*.png', '*.xml', '*/*.png']},
+       package_data = {pkg: ['*.png', '*.xml', '*/*.png', 'locale/*/LC_MESSAGES/*.mo']},
        data_files = [('/etc/enigma2/Foreca', ['plugin/City.cfg', 'plugin/Filter.cfg'])]
       )
