5700116bd63fc00eac668df84a838c4b58f6d1bb
JÃ¼rgen Strasdas <Software@Strasdas.de>
2012-04-15 10:53:31 +0200

    Add Italian localization and cities. Thanks to mat8861

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ import setup_translate
 
 pkg = 'Extensions.Foreca'
 setup (name = 'enigma2-plugin-extensions-foreca',
-       version = '2.0',
-       description = 'Foreca - Weather forecast',
+       version = '2.7',
+       description = 'Weather forecast for the upcoming 10 days',
        packages = [pkg],
        package_dir = {pkg: 'plugin'},
        package_data = {pkg: ['*.png', '*.xml', '*/*.png', 'locale/*/LC_MESSAGES/*.mo']},
