23ae033ac9e9be9259dbf4780b115d9b9b347c5c
ims21 <martin@volyne.cz>
2013-03-24 15:41:54 +0100

    Update setup.py
    increased version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setup_translate
 
 pkg = 'Extensions.Foreca'
 setup (name = 'enigma2-plugin-extensions-foreca',
-       version = '3.0.7',
+       version = '3.0.8',
        description = 'Weather forecast for the upcoming 10 days',
        packages = [pkg],
        package_dir = {pkg: 'plugin'},
