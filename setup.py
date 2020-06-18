225ff659425dfc718ab13f52014fd365f0a63bd2
jack2015 <jack2015@users.noreply.github.com>
2020-06-18 12:31:29 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup (name = 'enigma2-plugin-extensions-foreca',
        packages = [pkg],
        package_dir = {pkg: 'plugin'},
        package_data = {pkg: ['*.png', '*.xml', '*/*.png', 'locale/*/LC_MESSAGES/*.mo']},
-       data_files = [('/etc/enigma2/Foreca', ['plugin/City.cfg', 'plugin/Filter.cfg'])],
+       data_files = [('/etc/enigma2/Foreca', ['plugin/City.cfg', 'plugin/fav1.cfg', 'plugin/fav2.cfg', 'plugin/startservice.cfg', 'plugin/Filter.cfg'])],
        cmdclass = setup_translate.cmdclass, # for translation
       )
