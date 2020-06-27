408edd85d848b1530f3f8585816d748c55a0acaa
Jörg Bleyel <jbleyel@gmx.net>
2020-06-27 15:15:38 +0200

    Update merge

--- plugin/ui.py
+++ plugin/ui.py
@@ -13,11 +13,11 @@
 #
 from __future__ import absolute_import
 from __future__ import print_function
-VERSION = "3.2.0"
+VERSION = "3.2.3-r3"
 #
 #                    04.10.2017
 #
-#     Source of information: http://www.foreca.com
+#     Source of information: http://www.foreca.net
 #
 #             Design and idea by
 #                  @Bauernbub
@@ -101,6 +101,7 @@ VERSION = "3.2.0"
 #				self.valText4 = 365,87,600,28
 #	similar in user skin - there text4Pos="x,y,w,h" must be added
 # 3.2.0	fixed satellite maps, removed infrared - page not exist more, sanity check if nothing is downloaded
+# 3.2.3-r3 change URL to .net and .ru
 
 # Unresolved: Crash when scrolling in help screen of city panel
 #
@@ -186,7 +187,7 @@ config.plugins.foreca.time = ConfigSelection(default="24h", choices = [("12h", _
 config.plugins.foreca.debug = ConfigEnableDisable(default=False)
 
 
-MAIN_PAGE = "http://www.foreca.com"
+MAIN_PAGE = "http://www.foreca.net"
 USR_PATH = resolveFilename(SCOPE_CONFIG)+"Foreca"
 THUMB_PATH = resolveFilename(SCOPE_PLUGINS) + "Extensions/Foreca/thumb/"
 deviceName = HardwareInfo().get_device_name()
@@ -223,6 +224,8 @@ FILTERout = []
 FILTERidx = 0
 
 LANGUAGE = language.getActiveLanguage()[:2]
+if LANGUAGE == "zh":
+	LANGUAGE = "en"
 try:
 	locale.setlocale(locale.LC_COLLATE, language.getLanguage())
 except:
@@ -473,7 +476,7 @@ class MainMenuList(MenuList):
 		
 		# Text
 		x, y, w, h = self.valText1
-		self.res.append(MultiContentEntryText(pos=(x, y), size=(w, h), font=3, text=self.x[5], color=weiss, color_sel=weiss))
+		self.res.append(MultiContentEntryText(pos=(x, y), size=(w, h), font=3, text=_(self.x[5]), color=weiss, color_sel=weiss))
 		x, y, w, h = self.valText2
 		textsechs=self.x[6]
 		textsechs=textsechs.replace("&deg;", "") + tempUnit
@@ -544,7 +547,7 @@ class ForecaPreview(Screen, HelpableScreen):
 	def __init__(self, session):
 		global MAIN_PAGE, menu
 		self.session = session
-		MAIN_PAGE = "http://www.foreca.com"
+		MAIN_PAGE = "http://www.foreca.net"
 
 		# actual, local Time as Tuple
 		lt = localtime()
@@ -585,7 +588,7 @@ class ForecaPreview(Screen, HelpableScreen):
 			start = "London"
 		print(pluginPrintname, "home location:", self.ort)
 		
-		MAIN_PAGE = "http://www.foreca.com" + "/" + urllib.pathname2url(self.ort) + "?lang=" + LANGUAGE + "&details=" + heute + "&units=" + config.plugins.foreca.units.value +"&tf=" + config.plugins.foreca.time.value
+		MAIN_PAGE = "http://www.foreca.net" + "/" + urllib.pathname2url(self.ort) + "?lang=" + LANGUAGE + "&details=" + heute + "&units=" + config.plugins.foreca.units.value +"&tf=" + config.plugins.foreca.time.value
 		print(pluginPrintname, "initial link:", MAIN_PAGE)
 		
 		if HD:
@@ -835,7 +838,7 @@ class ForecaPreview(Screen, HelpableScreen):
 		jahr, monat, tag = lt[0:3]
 		morgen ="%04i%02i%02i" % (jahr, monat, tag)
 
-		MAIN_PAGE = "http://www.foreca.com" + "/" + urllib.pathname2url(self.ort) + "?lang=" + LANGUAGE + "&details=" + morgen + "&units=" + config.plugins.foreca.units.value + "&tf=" + config.plugins.foreca.time.value
+		MAIN_PAGE = "http://www.foreca.net" + "/" + urllib.pathname2url(self.ort) + "?lang=" + LANGUAGE + "&details=" + morgen + "&units=" + config.plugins.foreca.units.value + "&tf=" + config.plugins.foreca.time.value
 		print(pluginPrintname, "day link:", MAIN_PAGE)
 
 		# Show in GUI
@@ -898,7 +901,7 @@ class ForecaPreview(Screen, HelpableScreen):
 	def red(self):
 		if not self.working:
 			#/meteogram.php?loc_id=211001799&amp;mglang=de&amp;units=metrickmh&amp;tf=24h
-			self.url= "http://www.foreca.com" + "/meteogram.php?loc_id=" + self.loc_id + "&mglang=" + LANGUAGE + "&units=" + config.plugins.foreca.units.value + "&tf=" + config.plugins.foreca.time.value + "/meteogram.png"
+			self.url= "http://www.foreca.ru" + "/meteogram.php?loc_id=" + self.loc_id + "&mglang=" + LANGUAGE + "&units=" + config.plugins.foreca.units.value + "&tf=" + config.plugins.foreca.time.value + "/meteogram.png"
 			self.loadPicture(self.url)
 
 	def shift_red(self):
@@ -1527,7 +1530,7 @@ class SatPanel(Screen, HelpableScreen):
 		else:
 			# http://www.foreca.de/Austria/Linz?map=sat
 			devicepath = "/tmp/sat.html"
-			url = "http://www.foreca.com" + "/" + urllib.pathname2url(self.ort) + "?map=" + menu
+			url = "http://www.foreca.ru" + "/" + urllib.pathname2url(self.ort) + "?map=" + menu
 			# Load site for category and search Picture link
 			resp = urllib2.urlopen(url)
 			html = resp.read()
