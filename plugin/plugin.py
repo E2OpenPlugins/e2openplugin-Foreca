8d035db8eb92b5928671cfde3b838c4a781c4236
nautilus7 <spyros.n7@gmail.com>
2022-01-15 21:13:40 +0200

    Fix imports for python3

--- plugin/plugin.py
+++ plugin/plugin.py
@@ -6,7 +6,8 @@ from . import _
 from Plugins.Plugin import PluginDescriptor
 
 # Configuration
-from Components.config import *
+from Components.config import config, ConfigSubsection, ConfigEnableDisable
+
 config.plugins.foreca = ConfigSubsection()
 config.plugins.foreca.extmenu = ConfigEnableDisable(default=True)
 
@@ -21,15 +22,15 @@ def main(session, **kwargs):
 def Plugins(path, **kwargs):
 	global PICON_PATH
 	PICON_PATH = path + "/picon/"
-	list = [PluginDescriptor(name=_("Foreca Weather Forecast"),
+	plugin = [PluginDescriptor(name=_("Foreca Weather Forecast"),
 		description=_("Weather forecast for the upcoming 10 days"),
 		icon="foreca_logo.png",
 		where=PluginDescriptor.WHERE_PLUGINMENU,
 		fnc=main)]
 	if config.plugins.foreca.extmenu.value:
-		list.append(PluginDescriptor(name=_("Foreca Weather Forecast"),
+		plugin.append(PluginDescriptor(name=_("Foreca Weather Forecast"),
 		description=_("Weather forecast for the upcoming 10 days"),
 		icon="foreca_logo.png",
 		where=PluginDescriptor.WHERE_EXTENSIONSMENU,
 		fnc=main))
-	return list
+	return plugin
