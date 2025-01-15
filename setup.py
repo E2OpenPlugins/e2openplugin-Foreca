057789adcf87f1921b82f96d704dda79f435e5c5
jbleyel <jbleyel@gmx.net>
2025-01-15 10:52:58 +0100

    fix setup

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import setup_translate
 
 pkg = 'Extensions.Foreca'
 setup(name='enigma2-plugin-extensions-foreca',
-       version='3.2.8',
+       version='3.3.5',
        description='Weather forecast for the upcoming 10 days',
        packages=[pkg],
        package_dir={pkg: 'plugin'},
-       package_data={pkg: ['*.png', '*.xml', '*/*.png', 'locale/*/LC_MESSAGES/*.mo']},
-       data_files=[('/etc/enigma2/Foreca', ['plugin/City.cfg', 'plugin/Filter.cfg'])],
+       package_data={pkg: ['*.png', '*.txt', '*.xml', '*/*.png', '*/*.txt', 'locale/*/LC_MESSAGES/*.mo']},
+       data_files=[('/etc/enigma2/Foreca', ['plugin/City.cfg', 'plugin/Filter.cfg', 'plugin/fav1.cfg', 'plugin/fav2.cfg', 'plugin/startservice.cfg'])],
        cmdclass=setup_translate.cmdclass,  # for translation
       )
